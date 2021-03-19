b98543a34778e436721c29c5b6da160d5e752417
Ruby Quail <ruby@rubyquail.design>
2021-03-20 10:59:20 +1100

    init
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+import { fabric } from 'fabric';
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+canvas.width = 1280;
+canvas.height = 720;
+const { width: WID, height: HEI } = canvas;
+const ctx = canvas.getContext('2d')!;
+ctx.fillStyle = '#ff00ff';
+ctx.fillRect(0, 0, WID, HEI);
+const fab = new fabric.StaticCanvas(canvas);
+const rc = new fabric.Rect({ width: 100, height: 100, fill: 'red' });
+fab.add(rc);
+fab.renderAll();
+canvas.onmousemove = (e) => {
+  let x = e.offsetX;
+  let y = e.offsetY;
+  rc.set({ left: x, top: y });
+  fab.renderAll();
+};
